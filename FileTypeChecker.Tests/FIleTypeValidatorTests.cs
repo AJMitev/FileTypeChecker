7d109d7d664df4e5973c16410b0ea16a8f000746
AJMitev <mail@aleksandar.it>
2019-10-08 21:24:36 +0300

    Adding Tests project.

---
+++ FileTypeChecker.Tests/FIleTypeValidatorTests.cs
@@ -0,0 +1,95 @@
+﻿namespace FileTypeChecker.Tests
+{
+    using System;
+    using System.IO;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class FIleTypeValidatorTests
+    {
+        [Test]
+        public void IsTypeRecognizable_ShouldThrowArgumentNullExceptionIfStreamIsNull()
+            => Assert.Catch<ArgumentNullException>(() => FileTypeValidator.IsTypeRecognizable(null));
+
+        [Test]
+        public void IsTypeRecognizable_ShouldReturnFalseIfFormatIsUnknown()
+        {
+            using var fileStream = File.OpenRead("./files/test");
+
+            var expected = false;
+
+            var actual = FileTypeValidator.IsTypeRecognizable(fileStream);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [TestCase("./files/test.bmp")]
+        [TestCase("./files/test.jpg")]
+        [TestCase("./files/test.png")]
+        [TestCase("./files/test.gif")]
+        [TestCase("./files/test.tif")]
+        [TestCase("./files/test.psd")]
+        [TestCase("./files/test.pdf")]
+        [TestCase("./files/test.doc")]
+        [TestCase("./files/test.xml")]
+        [TestCase("./files/test.zip")]
+        [TestCase("./files/test.7z")]
+        public void IsTypeRecognizable_ShouldReturnTrueIfFileIsRecognized(string filePath)
+        {
+            using var fileStream = File.OpenRead(filePath);
+
+            var expected = true;
+
+            var actual = FileTypeValidator.IsTypeRecognizable(fileStream);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [TestCase("./files/test.bmp", "bmp")]
+        [TestCase("./files/test.jpg", "jpg")]
+        [TestCase("./files/test.png", "png")]
+        [TestCase("./files/test.gif", "gif")]
+        [TestCase("./files/test.tif", "tif")]
+        [TestCase("./files/test.psd", "psd")]
+        [TestCase("./files/test.pdf", "pdf")]
+        [TestCase("./files/test.doc", "doc")]
+        [TestCase("./files/test.xml", "xml")]
+        [TestCase("./files/test.zip", "zip")]
+        [TestCase("./files/test.7z", "7z")]
+        public void GetFileType_ShouldReturnFileExtension(string filePath, string expectedFileExtension)
+        {
+            using var fileStream = File.OpenRead(filePath);
+
+            var actualFileTypeExtension = FileTypeValidator.GetFileType(fileStream).Extension;
+
+            Assert.AreEqual(expectedFileExtension, actualFileTypeExtension);
+        }
+
+        [Test]
+        [TestCase("./files/test.bmp", "Bitmap")]
+        [TestCase("./files/test.jpg", "JPEG")]
+        [TestCase("./files/test.png", "Portable Network Graphic")]
+        [TestCase("./files/test.gif", "Graphics Interchange Format 89a")]
+        [TestCase("./files/test.tif", "Tagged Image File Format")]
+        [TestCase("./files/test.psd", "Photoshop Document file")]
+        [TestCase("./files/test.pdf", "Portable Document Format")]
+        [TestCase("./files/test.doc", "Microsoft Office Document 97-2003")]
+        [TestCase("./files/test.xml", "eXtensible Markup Language")]
+        [TestCase("./files/test.zip", "ZIP file")]
+        [TestCase("./files/test.7z", "7-Zip File Format")]
+        public void GetFileType_ShouldReturnFileName(string filePath, string expectedFileTypeName)
+        {
+            using var fileStream = File.OpenRead(filePath);
+
+            var actualFileTypeName = FileTypeValidator.GetFileType(fileStream).Name;
+
+            Assert.AreEqual(expectedFileTypeName, actualFileTypeName);
+        }
+
+        [Test]
+        public void GetFileType_ShouldThrowArgumentNullExceptionIfStreamIsNull()
+            => Assert.Catch<ArgumentNullException>(() => FileTypeValidator.GetFileType(null));
+    }
+}
