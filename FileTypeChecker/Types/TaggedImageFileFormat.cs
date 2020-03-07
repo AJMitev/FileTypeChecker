7a6bf4a09412e177dd5aee0b5727252edd6996fe
AJMitev <mail@aleksandar.it>
2020-03-07 19:10:29 +0200

    Changing tif extension

--- FileTypeChecker/Types/TaggedImageFileFormat.cs
+++ FileTypeChecker/Types/TaggedImageFileFormat.cs
@@ -5,7 +5,7 @@
     public class TaggedImageFileFormat : FileType, IFileType
     {
         private static readonly string name = "Tagged Image File Format";
-        private static readonly string extension = "tif/tiff";
+        private static readonly string extension = "tif";
         private static readonly byte[][] magicBytesJaggedArray = { new byte[] { 0x49, 0x49, 0x2A, 0x00 }, new byte[] { 0x4D, 0x4D, 0x00, 0x2A } };
 
         public TaggedImageFileFormat() : base(name, extension, magicBytesJaggedArray)
