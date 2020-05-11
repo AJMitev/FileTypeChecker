a418d3e9486a1e935ee3421e23cc9179fb9d4c2d
Aleksander J. Mitev <hello@aleksandar.it>
2020-05-11 22:34:43 +0300

    Adding web project to samples

---
+++ Samples/FileTypeChecker.WebApp/Models/InputModel.cs
@@ -0,0 +1,23 @@
+﻿namespace FileTypeChecker.WebApp.Models
+{
+    using FileTypeChecker.Web.Attributes;
+    using Microsoft.AspNetCore.Http;
+
+    public class InputModel
+    {
+        [AllowImageOnly]
+        public IFormFile FirstFile { get; set; }
+
+        [AllowArchiveOnly]
+        public IFormFile SecondFile { get; set; }
+
+        [AllowedTypes(FileExtension.Bitmap)]
+        public IFormFile ThirdFile { get; set; }
+
+        [ForbidExecutableFile]
+        public IFormFile FourthFile { get; set; }
+
+        [ForbidTypes(FileExtension.Doc)]
+        public IFormFile FifthFile { get; set; }
+    }
+}
