84f0340738323ede5fe93a76296021859d904d8d
AJMitev <hello@aleksandar.it>
2020-12-18 18:32:20 +0200

    Moving common logic to separate library.

--- FileTypeChecker.Common/DataValidator.cs
+++ FileTypeChecker.Common/DataValidator.cs
@@ -2,9 +2,9 @@
 {
     using System;
 
-    internal static class DataValidator
+    public static class DataValidator
     {
-        internal static void ThrowIfNull(object obj, string name)
+        public static void ThrowIfNull(object obj, string name)
         {
             if (obj == null)
             {
@@ -12,7 +12,7 @@
             }
         }
 
-        internal static void ThrowIfNullOrEmpty(string text, string name)
+        public static void ThrowIfNullOrEmpty(string text, string name)
         {
             if (string.IsNullOrEmpty(text))
             {
