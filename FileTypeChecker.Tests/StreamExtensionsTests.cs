0d333b53e088302efccd0fbf8154de506126542d
Aleksandar J. Mitev <hello@aleksandar.it>
2021-11-11 14:14:04 +0200

    Adding new tests

--- FileTypeChecker.Tests/StreamExtensionsTests.cs
+++ FileTypeChecker.Tests/StreamExtensionsTests.cs
@@ -4,6 +4,7 @@
     using FileTypeChecker.Extensions;
     using NUnit.Framework;
     using System.IO;
+    using System.Threading.Tasks;
 
     [TestFixture]
     public class StreamExtensionsTests
@@ -84,6 +85,46 @@
             using var fileStream = File.OpenRead("./files/test.bmp");
             var expected = false;
             var actual = fileStream.IsExecutable();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Document_ShouldReturnFalseIfTypesDidNotMatch()
+        {
+            using var fileStream = File.OpenRead("./files/test.bmp");
+            var expected = false;
+            var actual = fileStream.IsDocument();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IsDocument_ShouldReturnTrueIfTheTypesMatch()
+        {
+            using var fileStream = File.OpenRead("./files/test.doc");
+            var expected = true;
+            var actual = fileStream.IsDocument();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public async Task DocumentAsync_ShouldReturnFalseIfTypesDidNotMatch()
+        {
+            using var fileStream = File.OpenRead("./files/test.bmp");
+            var expected = false;
+            var actual = await fileStream.IsDocumentAsync();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public async Task IsDocumentAsync_ShouldReturnTrueIfTheTypesMatch()
+        {
+            using var fileStream = File.OpenRead("./files/test.doc");
+            var expected = true;
+            var actual = await fileStream.IsDocumentAsync();
 
             Assert.AreEqual(expected, actual);
         }
