93735e6d8a1fcb2cf32d9d6a47c4a038d19f56ea
ajmitev <ajmitev@pm.me>
2022-10-29 11:48:30 -0700

    Add support for heic files. Issue #21

--- FileTypeChecker/Types/HighEfficiencyImageFile.cs
+++ FileTypeChecker/Types/HighEfficiencyImageFile.cs
@@ -4,9 +4,14 @@
 
     public class HighEfficiencyImageFile : FileType, IFileType
     {
-        public const string TypeName = "High Efficiency Image File";
+        public const string TypeName = "High Efficiency Image File Format";
         public const string TypeExtension = "heic";
-        private static readonly MagicSequence magicBytes = new(new byte[] { 0x00, 0x00, 0x00, 0x20, 0x66, 0x74, 0x79, 0x70, 0x68, 0x65, 0x69, 0x63 });
+        private static readonly MagicSequence[] magicBytes =
+        {
+            new(new byte[] { 0x00, 0x00, 0x00, 0x20, 0x66, 0x74, 0x79, 0x70, 0x68, 0x65, 0x69, 0x63 }),
+            new(new byte[]{ 0x00, 0x00, 0x00, 0x18, 0x66, 0x74, 0x79, 0x70, 0x6d, 0x69, 0x66, 0x31 })
+        };
+
 
         public HighEfficiencyImageFile() : base(TypeName, TypeExtension, magicBytes)
         {
